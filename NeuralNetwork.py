a2eb51294ecb550de08a3b3f90515d3ae9e6fdea
Louis Spencer <louisspencer87@gmail.com>
2019-07-06 16:35:53 -0700

    working on improving accuracy of neural network

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,7 @@
 import numpy as np
 import keras
 from keras.utils import np_utils
+from sklearn.preprocessing import scale
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
@@ -24,12 +25,12 @@ class Network:
         for (layer_index, neurons) in enumerate(layers):
             if layer_index == 0:
                 self.weights.append(np.random.randn(neurons))
-                self.bias.append(np.zeros(neurons).reshape((neurons,1)))
+                self.bias.append(np.zeros(neurons))
                 prev_layer_neurons = neurons
                 continue
             else:
                 self.weights.append(np.array([np.random.randn(prev_layer_neurons) for _ in range(neurons)]))
-                self.bias.append(np.random.randn(neurons).reshape((neurons,1)))
+                self.bias.append(np.random.randn(neurons))
                 prev_layer_neurons = neurons
 
         self.weights = np.array(self.weights)
@@ -38,9 +39,8 @@ class Network:
     def predict(self, x):
         output_activation = self.feedforward(x, is_prediction = True)
         prediction_index = np.argmax(output_activation)
-        print(self.class_map)
-        print(self.class_map[prediction_index])
-
+        print("output_activation: {}, prediction: {}".format(output_activation, self.class_map[prediction_index]))
+        return self.class_map[prediction_index]
 
     def train(self, X, y, lr = 0.01, batch_size = 100, epochs = 100):
         #w = w - lr/m * sum{from 1 to m} ()
@@ -49,7 +49,8 @@ class Network:
         self.create_class_mapping(y)
 
         activations = [np.array([])]
-        true_batch = min(batch_size, len(X))
+        X_len = len(X)
+        true_batch = min(batch_size, X_len)
         if true_batch < 100:
             batch_size = true_batch
 
@@ -74,26 +75,29 @@ class Network:
             for (x_train_example, y_train_example) in zip(training_batch_X, mapped_y):
                 activations, weighted_inputs = self.feedforward(x_train_example)
                 errors = self.backpropagation(y_train_example, activations, weighted_inputs)
-                self.update_weights(activations, errors)
+                self.update_weights(activations, errors, lr, batch_size)
 
                 error += (np.linalg.norm((activations[-1] - y_train_example))) ** 2
 
-            print("Epoch: {}, Error: {}".format(epoch, error))
+            print("Epoch: {}, Error: {}".format(epoch + 1, error))
 
     def feedforward(self, input, is_prediction = False):
-        input = input.reshape((input.shape[0] * input.shape[1], 1))
+        # input = input.reshape((input.shape[0] * input.shape[1], 1))
         activations = []
         weighted_inputs = []
         for (layer_index, neurons) in enumerate(self.layers):
             if layer_index == 0:
                 input_weights_shape = self.weights[0].shape
-                weighted_input = self.weights[0].reshape((input_weights_shape[0], 1)) * input
+                weighted_input = self.weights[0] * input
                 activations.append(sigmoid(weighted_input))
                 weighted_inputs.append(weighted_input)
             else:
                 weighted_input = np.dot(self.weights[layer_index], activations[layer_index - 1]) + self.bias[layer_index]
                 activations.append(sigmoid(weighted_input))
                 weighted_inputs.append(weighted_input)
+
+        activations = np.array(activations)
+        weighted_inputs = np.array(weighted_inputs)
 
         if is_prediction:
             return activations[-1]
@@ -117,22 +121,17 @@ class Network:
                 errors[layer_index] = curr_layer_error
         return errors
 
-    def update_weights(self, activations, errors):
+    def update_weights(self, activations, errors, lr, batch_size):
         for layer_index in range(1, len(self.layers)):
             # for weight_matrix_row in range(self.layers[layer_index]):
             error_layer = len(errors[layer_index])
             layer_l_minus_1_neuron_count = self.layers[layer_index - 1]
-            dC_dw = np.array([[activations[layer_index - 1][row] * errors[layer_index][col]
-            for col in range(error_layer)]
+            dC_dw = np.array([[activations[layer_index - 1][row] * errors[layer_index][col] for col in range(error_layer)]
             for row in range(layer_l_minus_1_neuron_count)])
             dC_db = errors[layer_index]
 
-            print("layer index: ", layer_index)
-            print(self.weights[layer_index].shape, dC_dw.T.shape)
-
-            self.weights[layer_index] -= dC_dw.T
-            self.bias[layer_index] -= dC_db
-
+            self.weights[layer_index] -= (lr / batch_size * dC_dw.T)
+            self.bias[layer_index] -= (lr / batch_size * dC_db)
 
     def create_class_mapping(self, y):
         class_map = {}
@@ -148,7 +147,21 @@ class Network:
         self.class_map = class_map
         self.inverse_map = inverse_map
 
-n = Network([784,1,10])
+n = Network([784,10,10,10,10])
 (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
-X_train = list(map(lambda x: x.reshape((784, 1)), X_train))
-n.train(X_train, y_train, batch_size = 300, epochs = 1)
+
+X_train = list(map(lambda x: x.reshape((784,)), X_train))
+X_train = scale(X_train)
+
+X_test = list(map(lambda x: x.reshape((784,)), X_test))
+X_test = scale(X_test)
+
+n.train(X_train, y_train, batch_size = 1, epochs = 1000, lr = 0.01)
+num_correct = 0
+for i in range(len(X_test)):
+    pred = n.predict(X_test[i])
+    print("actual: ", y_test[i])
+    if pred == y_test[i]:
+        num_correct += 1
+accuracy = num_correct / len(X_test)
+print("accuracy: {}".format(accuracy))
