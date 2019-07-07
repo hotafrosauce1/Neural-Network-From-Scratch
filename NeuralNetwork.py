03a70f303aae8086e698a896563feee5dcb8454f
Louis Spencer <louisspencer87@gmail.com>
2019-07-07 12:05:41 -0700

    starting to debug backpropagation method

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,7 @@
 import numpy as np
 import keras
 from keras.utils import np_utils
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import normalize
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
@@ -24,13 +24,12 @@ class Network:
         prev_layer_neurons = None
         for (layer_index, neurons) in enumerate(layers):
             if layer_index == 0:
-                self.weights.append(np.random.randn(neurons))
+                self.weights.append(np.ones(neurons))
                 self.bias.append(np.zeros(neurons))
                 prev_layer_neurons = neurons
-                continue
             else:
                 self.weights.append(np.array([np.random.randn(prev_layer_neurons) for _ in range(neurons)]))
-                self.bias.append(np.random.randn(neurons))
+                self.bias.append(np.random.rand(neurons))
                 prev_layer_neurons = neurons
 
         self.weights = np.array(self.weights)
@@ -39,8 +38,7 @@ class Network:
     def predict(self, x):
         output_activation = self.feedforward(x, is_prediction = True)
         prediction_index = np.argmax(output_activation)
-        print("output_activation: {}, prediction: {}".format(output_activation, self.class_map[prediction_index]))
-        return self.class_map[prediction_index]
+        return self.class_map[prediction_index], output_activation
 
     def train(self, X, y, lr = 0.01, batch_size = 100, epochs = 100):
         #w = w - lr/m * sum{from 1 to m} ()
@@ -48,25 +46,18 @@ class Network:
 
         self.create_class_mapping(y)
 
-        activations = [np.array([])]
         X_len = len(X)
         true_batch = min(batch_size, X_len)
         if true_batch < 100:
             batch_size = true_batch
 
         for epoch in range(epochs):
-            holder = [(X[row], y[row]) for row in range(batch_size)]
-
-            y = np.reshape(y, (len(y), 1))
-            X_and_y = np.array(holder)
+            X_and_y = np.c_[X, y]
 
             np.random.shuffle(X_and_y)
 
-            X = [X_and_y[i][0] for i in range(batch_size)]
-            y = [X_and_y[i][1] for i in range(batch_size)]
-
-            training_batch_X = X[:batch_size]
-            training_batch_y = y[:batch_size]
+            training_batch_X = X_and_y[:batch_size,:-1]
+            training_batch_y = X_and_y[:batch_size,-1]
 
             mapped_outputs = np.array(list(map(lambda x: self.inverse_map[x], training_batch_y)))
             mapped_y = np_utils.to_categorical(mapped_outputs, num_classes = self.num_classes)
@@ -82,12 +73,10 @@ class Network:
             print("Epoch: {}, Error: {}".format(epoch + 1, error))
 
     def feedforward(self, input, is_prediction = False):
-        # input = input.reshape((input.shape[0] * input.shape[1], 1))
         activations = []
         weighted_inputs = []
         for (layer_index, neurons) in enumerate(self.layers):
             if layer_index == 0:
-                input_weights_shape = self.weights[0].shape
                 weighted_input = self.weights[0] * input
                 activations.append(sigmoid(weighted_input))
                 weighted_inputs.append(weighted_input)
@@ -108,9 +97,7 @@ class Network:
         errors = {}
         num_layers = len(self.layers)
         for layer_index in range(num_layers - 1, 0, -1):
-            if layer_index == 0:
-                return errors
-            elif layer_index == num_layers - 1:
+            if layer_index == num_layers - 1:
                 gradient_cost = activations[layer_index] - y
                 errors[layer_index] = gradient_cost * sigmoid_prime(weighted_inputs[layer_index])
             else:
@@ -123,11 +110,13 @@ class Network:
 
     def update_weights(self, activations, errors, lr, batch_size):
         for layer_index in range(1, len(self.layers)):
-            # for weight_matrix_row in range(self.layers[layer_index]):
             error_layer = len(errors[layer_index])
             layer_l_minus_1_neuron_count = self.layers[layer_index - 1]
-            dC_dw = np.array([[activations[layer_index - 1][row] * errors[layer_index][col] for col in range(error_layer)]
+
+            dC_dw = np.array([[activations[layer_index - 1][row] * errors[layer_index][col]
+            for col in range(error_layer)]
             for row in range(layer_l_minus_1_neuron_count)])
+
             dC_db = errors[layer_index]
 
             self.weights[layer_index] -= (lr / batch_size * dC_dw.T)
@@ -138,7 +127,7 @@ class Network:
         inverse_map = {}
         curr_class_num = 0
         for val in np.unique(y):
-            if val in class_map.keys() and val in inverse_map.keys():
+            if val in class_map.keys() and val in inverse_map.values():
                 continue
             else:
                 class_map[curr_class_num] = val
@@ -147,21 +136,21 @@ class Network:
         self.class_map = class_map
         self.inverse_map = inverse_map
 
-n = Network([784,10,10,10,10])
+n = Network([784,30,10,10])
 (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
 
 X_train = list(map(lambda x: x.reshape((784,)), X_train))
-X_train = scale(X_train)
+X_train = normalize(X_train)
 
 X_test = list(map(lambda x: x.reshape((784,)), X_test))
-X_test = scale(X_test)
+X_test = normalize(X_test)
 
-n.train(X_train, y_train, batch_size = 1, epochs = 1000, lr = 0.01)
+n.train(X_train, y_train, batch_size = 100, epochs = 10, lr = 0.01)
 num_correct = 0
 for i in range(len(X_test)):
-    pred = n.predict(X_test[i])
-    print("actual: ", y_test[i])
+    pred, activation = n.predict(X_test[i])
+    print("actual: ", y_test[i], "predicted: ", pred, "\n","activation: ", activation)
     if pred == y_test[i]:
         num_correct += 1
 accuracy = num_correct / len(X_test)
-print("accuracy: {}".format(accuracy))
+print("accuracy: {}%".format(accuracy * 100))
--- NeuralNetworkTests.py
+++ NeuralNetworkTests.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class NeuralNetworkTest:
 
-    net = NeuralNetwork.Network([3,3,2,10])
+    net = NeuralNetwork.Network([3,30,30,3])
 
     def test_dimensions(self):
         for layer_index, neuron_count in enumerate(self.net.layers):
@@ -15,7 +15,7 @@ class NeuralNetworkTest:
                 actual_weight_dim = self.net.weights[layer_index].shape
                 assert expected_weight_dim == actual_weight_dim, "expected: {}, actual: {}".format(expected_weight_dim, actual_weight_dim)
 
-                expected_bias_dim = (neuron_count, 1)
+                expected_bias_dim = (neuron_count, )
                 actual_bias_dim = self.net.bias[layer_index].shape
                 assert expected_bias_dim == actual_bias_dim, "expected: {}, actual: {}".format(expected_bias_dim, actual_bias_dim)
 
@@ -28,7 +28,7 @@ class NeuralNetworkTest:
                 actual_weight_dim = self.net.weights[layer_index].shape
                 assert actual_weight_dim == expected_weight_dim, "expected: {}, actual: {}".format(expected_weight_dim, actual_weight_dim)
 
-                expected_bias_dim = (neuron_count, 1)
+                expected_bias_dim = (neuron_count, )
                 actual_bias_dim = self.net.bias[layer_index].shape
                 assert expected_bias_dim == actual_bias_dim, "expected: {}, actual: {}".format(expected_bias_dim, actual_bias_dim)
 
@@ -37,10 +37,39 @@ class NeuralNetworkTest:
             assert type(self.net.weights[layer_index]) is np.ndarray
             assert type(self.net.bias[layer_index]) is np.ndarray
 
+    def test_predictions(self):
+        y = []
+        X = []
+        for _ in range(22):
+            X.append([1,2,3])
+            y.append(0)
 
+        for _ in range(22):
+            X.append([4,5,6])
+            y.append(1)
 
+        for _ in range(22):
+            X.append([7,8,9])
+            y.append(2)
+
+        X = np.array(X)
+        y = np.array(y)
+
+        self.net.train(X, y, epochs = 500)
+
+        correct = 0
+
+        Xandy = np.c_[X, y]
+        for _ in range(100):
+            np.random.shuffle(Xandy)
+            x = Xandy[:,:-1][0]
+            actual = Xandy[:,-1][0]
+            pred, act = self.net.predict(x)
+            print("input: ", x, "prediction: ", pred, "actual: ", actual ,"activation output: ", act)
+            if actual == pred:
+                correct += 1
+        print("accuracy: {}%".format(correct))
 
 
 test = NeuralNetworkTest()
-test.test_dimensions()
-test.test_variable_types()
+test.test_predictions()
